09e281d9629d87989cc2808404b0a2c995b03c46
usmanmirCS <47401816+usmanmirCS@users.noreply.github.com>
2020-12-17 18:01:17 -0700

    Added Sim Shoot
    A script that allows our sim hand to shoot orange spheres called fireballs

---
+++ Dec AN 2020/Assets/Scripts/SimHand/SimShoot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimShoot : MonoBehaviour
+{
+    public GameObject m_prefabFireball;
+    public Transform m_spawnTransform;
+    public float m_shootForce = 250f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            //Instantiate
+            GameObject fireball = Instantiate(m_prefabFireball, m_spawnTransform.position, m_spawnTransform.rotation);
+            //add force to it
+            fireball.GetComponent<Rigidbody>().AddForce(m_spawnTransform.forward * m_shootForce);
+            //Destroy it after sometime
+            Destroy(fireball, 5f);
+        }
+    }
+}
