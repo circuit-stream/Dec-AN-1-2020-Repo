4040109c1c60f76ec6f118f783437ac989ba29aa
usmanmirCS <47401816+usmanmirCS@users.noreply.github.com>
2020-12-30 18:16:08 -0700

    Session 8

---
+++ Dec AN 2020/Assets/Scripts/SimHand/SimGrab.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimGrab : MonoBehaviour
+{
+    public Animator m_anim;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            m_anim.SetBool("isGrabbing", true);
+        }
+        if(Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            m_anim.SetBool("isGrabbing", false);
+        }
+    }
+}
