0ce959ca23ea077820399b9f1cd327cf8b5ee747
usmanmirCS <47401816+usmanmirCS@users.noreply.github.com>
2021-01-07 20:14:13 -0700

    Session 10

---
+++ Dec AN 2020/Assets/Scripts/Interactions/TouchButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Events;
+
+public class TouchButton : MonoBehaviour
+{
+    public Transform m_downTransform;
+    public Transform m_buttonVisual;
+    public UnityEvent m_btnDown;
+    public UnityEvent m_btnUp;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            m_buttonVisual.position = m_downTransform.position;
+            m_btnDown.Invoke();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            m_buttonVisual.localPosition = Vector3.zero;
+            m_btnUp.Invoke();
+        }
+    }
+}
---
+++ Dec AN 2020/Assets/Scripts/VR/Haptics.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Haptics : MonoBehaviour
+{
+    public XRNode m_leftNode;
+    public XRNode m_rightNode;
+    private static InputDevice m_vrLeftController;
+    private static InputDevice m_vrRighttController;
+
+    private void Awake()
+    {
+        m_vrLeftController = InputDevices.GetDeviceAtXRNode(m_leftNode);
+        m_vrRighttController = InputDevices.GetDeviceAtXRNode(m_rightNode);
+    }
+
+    public static void VibrateLeft()
+    {
+        m_vrLeftController.SendHapticImpulse(0, 0.25f, 1f);
+    }
+    public static void VibrateRight()
+    {
+        m_vrRighttController.SendHapticImpulse(0, 0.25f, 1f);
+    }
+}
