e2c8afdb4c8085446a76c8c1764836bdca153776
usmanmirCS <47401816+usmanmirCS@users.noreply.github.com>
2021-01-19 18:11:11 -0700

    session 13

--- Dec AN 2020/Assets/Scripts/SimHand/SimGrab.cs
+++ Dec AN 2020/Assets/Scripts/SimHand/SimGrab.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class SimGrab : MonoBehaviour
 {
     public Animator m_anim;
+    public float m_throwForce = 25;
 
     private GameObject m_touchingObject;
     private GameObject m_heldObject;
+
+    public List<Vector3> m_grabPosition = new List<Vector3>();
 
     private void OnTriggerStay(Collider other)
     {
@@ -28,7 +31,7 @@ public class SimGrab : MonoBehaviour
             m_anim.SetBool("isGrabbing", true);
             if(m_touchingObject)
             {
-                Grab();
+                AdvGrab();
             }
         }
         if(Input.GetKeyUp(KeyCode.Mouse1))
@@ -36,7 +39,20 @@ public class SimGrab : MonoBehaviour
             m_anim.SetBool("isGrabbing", false);
             if(m_heldObject)
             {
-                Release();
+                AdvRelease();
+            }
+        }
+
+        if(m_heldObject)
+        {
+            if(m_grabPosition.Count < 20)
+            {
+                m_grabPosition.Add(this.transform.position);
+            }
+            else
+            {
+                m_grabPosition.RemoveAt(0);
+                m_grabPosition.Add(this.transform.position);
             }
         }
     }
@@ -52,6 +68,40 @@ public class SimGrab : MonoBehaviour
     {
         m_heldObject.transform.SetParent(null);
         m_heldObject.GetComponent<Rigidbody>().isKinematic = false;
+        m_heldObject = null;
+    }
+
+
+    void AdvGrab()
+    {
+        m_heldObject = m_touchingObject;
+        m_heldObject.transform.SetParent(this.transform);
+        FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+        fx.connectedBody = m_heldObject.GetComponent<Rigidbody>();
+        fx.breakForce = 10000;
+        fx.breakTorque = 10000;
+    }
+
+    void AdvRelease()
+    {
+        Destroy(GetComponent<FixedJoint>());
+        m_heldObject.transform.SetParent(null);
+
+
+        Vector3 dir = m_grabPosition[m_grabPosition.Count - 1] - m_grabPosition[0];
+
+        Rigidbody rb = m_heldObject.GetComponent<Rigidbody>();
+
+        rb.AddForce(dir * m_throwForce);
+        m_grabPosition.Clear();
+
+
+        m_heldObject = null;
+    }
+
+    private void OnJointBreak(float breakForce)
+    {
+        m_heldObject.transform.SetParent(null);
         m_heldObject = null;
     }
 }
--- Dec AN 2020/Assets/Scripts/VR/VRGrab.cs
+++ Dec AN 2020/Assets/Scripts/VR/VRGrab.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class VRGrab : MonoBehaviour
 {
+    public float m_throwForce;
     public Animator m_anim;
 
     public string m_grip;
@@ -12,6 +13,7 @@ public class VRGrab : MonoBehaviour
     GameObject m_touchingObject;
     GameObject m_heldObject;
 
+    public List<Vector3> m_grabPositions = new List<Vector3>();
     private void OnTriggerStay(Collider other)
     {
         if(other.tag == "Interactable")
@@ -33,7 +35,7 @@ public class VRGrab : MonoBehaviour
             m_anim.SetBool("isGrabbing", true);
             if(m_touchingObject)
             {
-                Grab();
+                AdvGrab();
             }
         }
         else if(Input.GetAxis(m_grip) < 0.8f && m_gripHeld == true)
@@ -42,7 +44,23 @@ public class VRGrab : MonoBehaviour
             m_anim.SetBool("isGrabbing", false);
             if(m_heldObject)
             {
-                Release();
+                AdvRelease();
+            }
+        }
+
+        if(m_heldObject)
+        {
+            Debug.Log("1");
+            if(m_grabPositions.Count < 20)
+            {
+                Debug.Log("2");
+                m_grabPositions.Add(this.transform.position);
+            }
+            else
+            {
+                Debug.Log("3");
+                m_grabPositions.RemoveAt(0);
+                m_grabPositions.Add(this.transform.position);
             }
         }
     }
@@ -58,6 +76,36 @@ public class VRGrab : MonoBehaviour
     {
         m_heldObject.transform.SetParent(null);
         m_heldObject.GetComponent<Rigidbody>().isKinematic = false;
+        m_heldObject = null;
+    }
+
+    void AdvGrab()
+    {
+        m_heldObject = m_touchingObject;
+        m_heldObject.transform.SetParent(this.transform);
+        FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+        fx.breakForce = 10000;
+        fx.breakTorque = 10000;
+    }
+
+    void AdvRelease()
+    {
+        Destroy(GetComponent<FixedJoint>());
+        m_heldObject.transform.SetParent(null);
+
+        Vector3 dir = m_grabPositions[m_grabPositions.Count - 1] - m_grabPositions[0];
+
+        Rigidbody rb = m_heldObject.GetComponent<Rigidbody>();
+        rb.AddForce(dir * m_throwForce);
+
+        m_grabPositions.Clear();
+
+        m_heldObject = null;
+    }
+
+    private void OnJointBreak(float breakForce)
+    {
+        m_heldObject.transform.SetParent(null);
         m_heldObject = null;
     }
 }
