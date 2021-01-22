fe33905114cf7f7d101c967491e6ae4590ccf2f5
usmanmirCS <47401816+usmanmirCS@users.noreply.github.com>
2021-01-21 18:17:14 -0700

    Session 14

---
+++ Dec AN 2020/Assets/Scripts/VR/VRRig.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class VRRig : MonoBehaviour
+{
+    public SphereCollider m_feetCollider;
+
+    public Transform m_vrHead;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        XRDevice.SetTrackingSpaceType(TrackingSpaceType.RoomScale);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 feetCollisionSpot = m_vrHead.transform.localPosition;
+        feetCollisionSpot.y = 0.1f;
+        m_feetCollider.center = feetCollisionSpot;
+    }
+}
