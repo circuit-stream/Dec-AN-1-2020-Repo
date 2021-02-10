f3b956dcabc21c4158f8aa2c764a199bbfedc579
usmanmirCS <47401816+usmanmirCS@users.noreply.github.com>
2021-02-09 18:49:57 -0700

    Session 19

---
+++ Dec AN 2020/Assets/Scripts/Interactions/Dial.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dial : MonoBehaviour
+{
+    public Transform m_lookAtTarget, m_targetReset;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            m_lookAtTarget.parent = other.transform;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Vector3 lookPosition = m_lookAtTarget.position - transform.position;
+            lookPosition.y = 0;
+            Quaternion rot = Quaternion.LookRotation(lookPosition);
+            transform.rotation = rot;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            m_lookAtTarget.parent = transform;
+            m_lookAtTarget.position = m_targetReset.position;
+        }
+    }
+}
---
+++ Dec AN 2020/Assets/Scripts/Interactions/SlideLever.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideLever : MonoBehaviour
+{
+    public Transform m_start, m_end;
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Vector3 heading = m_end.position - m_start.position;
+            float magnitudeOfHeading = heading.magnitude;
+            heading.Normalize();
+
+            Vector3 startToHand = other.transform.position - m_start.position;
+            float dotProduct = Vector3.Dot(startToHand, heading);
+            dotProduct = Mathf.Clamp(dotProduct, 0, magnitudeOfHeading);
+
+            transform.position = m_start.position + heading * dotProduct;
+        }
+    }
+}
